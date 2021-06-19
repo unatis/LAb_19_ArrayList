9fd1d8a81321ec4a8d8e3a9b506d99652f123d5f
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-19 17:54:25 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> cars = new ArrayList<String>();
+
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+
+        System.out.println(cars);
+
+        cars.remove("BMW");
+        cars.remove(2);
+
+        for(String Car : cars){
+            System.out.println(Car);
+        }
+
+
+    }
+
+}
